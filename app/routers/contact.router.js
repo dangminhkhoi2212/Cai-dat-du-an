c64a6ae3e128ce8cff65c8740f06afa031f0a07d
dangminhkhoi2212 <kkhoi600@gmail.com>
2023-02-04 23:19:12 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routers/contact.router.js
+++ app/routers/contact.router.js
@@ -7,7 +7,7 @@ router
     .route('/')
     .get(contacts.findAll)
     .post(contacts.create)
-    .delete(contacts.deleteALL);
+    .delete(contacts.deleteAll);
 router.route('/favorite').get(contacts.findAllFavorite);
 router
     .route('/:id')
